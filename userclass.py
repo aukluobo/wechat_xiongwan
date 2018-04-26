2f2d18a903239a9ce11556ad488b9bc71c249f44
[huangguodong] <[huangguodong@genomics.cn>
2018-04-26 16:01:27 +0800

    add timecap set

--- userclass.py
+++ userclass.py
@@ -3,6 +3,7 @@ import sys
 
 class user:
     def __init__(self):
+        self.time='1m' # 1h 是一个小时，1m是一分钟，以此类推
         self.user=[] #设置你想要发的人，写他在你在微信上的备注名，用单引号括起来，多个人用逗号分开,注意标点符号要在“英文状态”下输入，不然会报错
         self.userSepecial={}  #默认发送下面的message，但是可以在这里给特定的人设置不一样的信息
         self.message="快交表！！"
